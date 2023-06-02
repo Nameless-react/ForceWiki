1794e18d2915c0cbbe1cd4b5f82deabb8e88b813
Joel <joel33960gmail.com>
2023-06-02 11:46:49 -0600

    Primer commit de la ForceWiki

---
+++ src/main/java/com/ForceWiki/ForceWikiApplication.java
@@ -0,0 +1,13 @@
+package com.ForceWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForceWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForceWikiApplication.class, args);
+	}
+
+}
